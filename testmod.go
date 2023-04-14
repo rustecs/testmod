beaf8fd588f7022d008d092c5fac348d014f812d
rustecs <ruslan.tecs@gmail.com>
2023-04-14 15:26:14 +0300

    add ru

--- testmod.go
+++ testmod.go
@@ -13,9 +13,11 @@ func Hi(name, lang string) (string, error) {
     case "pt":
         return fmt.Sprintf("Oi, %s!", name), nil
     case "es":
-        return fmt.Sprintf("¡Hola, %s!", name), nil
+        return fmt.Sprintf("Hola, %s!", name), nil
     case "fr":
         return fmt.Sprintf("Bonjour, %s!", name), nil
+    case "ru":
+        return fmt.Sprintf("Привет, %s!", name), nil
     default:
         return "", errors.New("unknown language")
     }
